d5a3f7ead23f8bd885378ed88c7edc9d28c257b6
carstengieshoff <carsten.gieshoff@rwth-aachen.de>
2022-02-08 10:38:42 +0100

    Add reader for nature dataset and add necessary dependecies

---
+++ data_handling/nature_reader.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+from typing import Any, Dict, List, Literal, Union
+
+import numpy as np
+import pandas as pd
+
+from data_handling.data_reader import DataPoint, DataReader
+
+DataType = Literal["noisy", "clean"]
+
+
+class NatureReader(DataReader):
+    def __init__(self, path: Union[str, Path], use: DataType = "noisy"):
+        super().__init__(path=path)
+        self._use = use
+        self._info = self._read_info()
+
+    def get_dataset(self) -> List[DataPoint]:
+        return [
+            DataPoint(self.get_data_from_reference(ref), self.get_label_from_reference(ref))
+            for ref in self._info.keys()
+        ]
+
+    def get_data_from_reference(self, ref: str) -> np.ndarray:
+        self._check_ref_exists(ref=ref)
+        extension = "ECGData"
+        if self._use == "clean":
+            extension += "Denoised"
+
+        df = pd.read_csv(self._path / extension / f"{ref}.csv")
+        data = df.values
+        return data
+
+    def get_label_from_reference(self, ref: str) -> "str":
+        self._check_ref_exists(ref=ref)
+        label: str = self._info[ref]["Rhythm"]
+        return label
+
+    def _read_info(self) -> Dict[str, Dict[str, Any]]:
+        info_df = pd.read_excel(self._path / "Diagnostics.xlsx")
+        info_df.set_index("FileName", inplace=True)
+        info: Dict[str, Dict[str, Any]] = info_df.T.to_dict()
+        return info
+
+    def _check_ref_exists(self, ref: str) -> None:
+        if ref not in self._info.keys():
+            raise KeyError(f"Got unknown reference: `{ref}`")

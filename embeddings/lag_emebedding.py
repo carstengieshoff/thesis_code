ba48bdc72fe2dd44df99fa8f5ac5adf684c5723d
carstengieshoff <carsten.gieshoff@rwth-aachen.de>
2022-02-10 12:17:15 +0100

    Add LagEmbedding for multivariate signals and add tests for the embedding

--- embeddings/lag_emebedding.py
+++ embeddings/lag_emebedding.py
@@ -16,15 +16,26 @@ class LagEmbedding(Embedding):
     """Embedding a signal by replacing each datapoint by a set of (past) data points."""
 
     def __init__(self, dim: int, lag: int):
+        assert dim >= 1, "`dim` is expected to be a positive integer"
         self._dim = dim
         self._lag = lag
 
     def embedd(self, signal: np.array) -> np.array:
 
-        embedded_signal = sliding_window_view(signal, window_shape=self._dim * self._lag, axis=0).squeeze()
-        embedded_signal = embedded_signal[:, 0 :: self._lag]
+        if len(signal.shape) >= 3:
+            raise NotImplementedError("LagEmbedding for 3d signals not yet implemented")
 
-        return embedded_signal
+        if self._lag == 0 or self._dim == 1:
+            return signal
+
+        embedded_signal = sliding_window_view(signal, window_shape=self._dim * self._lag, axis=0).squeeze()
+
+        if signal.shape[1] > 1:
+            embedded_signal = embedded_signal[:, :, self._lag * (1 - self._dim) - 1 :: self._lag]
+        else:
+            embedded_signal = embedded_signal[:, self._lag * (1 - self._dim) - 1 :: self._lag]
+
+        return embedded_signal.reshape(-1, signal.shape[1], self._dim).squeeze()
 
 
 if __name__ == "__main__":
---
+++ tests/test_embeddings/test_lag_embedding.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pytest
+
+from embeddings.lag_emebedding import LagEmbedding
+
+
+@pytest.mark.parametrize("size", [100, 101])
+@pytest.mark.parametrize("lag", [1, 2, 3, 4, 5])
+@pytest.mark.parametrize("dim", [1, 2, 3, 4, 5])
+def test_lag_embedding_for_1d_signals(size: int, lag: int, dim: int) -> None:
+    # given
+    signal = np.random.random(size=(size, 1))
+    embedding = LagEmbedding(lag=lag, dim=dim)
+    # when
+    embedded_signal = embedding.embedd(signal=signal)
+
+    # then
+    if dim == 1:
+        expected_shape = signal.shape
+    else:
+        expected_shape = (signal.shape[0] - lag * dim + 1, dim)
+
+    # then
+    assert embedded_signal.shape == expected_shape
+    #  -1 ist the last position (and the first dimension), dim-1 lags still miss and mus be added (subtracted)
+    expected_last_embedding = signal[lag * (1 - dim) - 1 :: lag, :].reshape(-1, 1)
+    actual_last_embedding = embedded_signal[-1, :].reshape(-1, 1)
+    assert np.array_equal(actual_last_embedding, expected_last_embedding)
+
+
+@pytest.mark.parametrize("size", [100, 101])
+@pytest.mark.parametrize("lag", [1, 2, 3, 4, 5])
+@pytest.mark.parametrize("dim", [1, 2, 3, 4, 5])
+def test_lag_embedding_for_2d_signals(size: int, lag: int, dim: int) -> None:
+    # given
+    signal = np.random.random(size=(size, 2))
+    embedding = LagEmbedding(lag=lag, dim=dim)
+    # when
+    embedded_signal = embedding.embedd(signal=signal)
+
+    # then
+    if dim == 1:
+        expected_shape = signal.shape
+    else:
+        expected_shape = (signal.shape[0] - lag * dim + 1, 2, dim)
+
+    # then
+    assert embedded_signal.shape == expected_shape
+    #  -1 ist the last position (and the first dimension), dim-1 lags still miss and mus be added (subtracted)
+    expected_last_embedding = signal[lag * (1 - dim) - 1 :: lag, :].T.squeeze()
+    actual_last_embedding = embedded_signal[-1, :]
+    assert np.array_equal(actual_last_embedding, expected_last_embedding)

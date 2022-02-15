406a8df795fd51545d0547c573db51f0a4d8002e
carstengieshoff <carsten.gieshoff@rwth-aachen.de>
2022-02-15 10:44:25 +0100

    Replace metrics by usage of scipy.spatial.distance.cdist.
    Documentation:
    https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.cdist.html
    
    This achieves significant better performance than other options that were explored thus far.

--- metrics/__init__.py
+++
@@ -1,4 +0,0 @@
-from metrics.cosine_distance import cosine_dist
-from metrics.minkowski_distance import minkowski_dist
-
-__all__ = ["minkowski_dist", "cosine_dist"]
--- recurrence_plots/recurrence_plot.py
+++ recurrence_plots/recurrence_plot.py
@@ -1,13 +1,16 @@
-from typing import Any, Callable
+from typing import Any, Callable, Union
 
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.spatial.distance import cdist
 
 from embeddings.lag_emebedding import Embedding
 
 
 class RecurrencePlot:
-    def __init__(self, signal: np.array, metric: Callable[[np.array, np.array], float], embedding: Embedding):
+    def __init__(
+        self, signal: np.array, metric: Union[Callable[[np.array, np.array], np.ndarray], str], embedding: Embedding
+    ):
         self.original_signal = signal.copy()
         self.embedded_signal = embedding.embedd(self.original_signal)
         self.metric = metric
@@ -15,16 +18,14 @@ class RecurrencePlot:
         self.num_data_points, *self.shape_data_points = self.embedded_signal.shape
         self._recurrence_plot = np.zeros(shape=(self.num_data_points, self.num_data_points))
 
-    def generate(self, normalize: bool = True) -> np.array:
+    def generate(self, normalize: bool = True) -> np.ndarray:
         """Generating a RP from the provided signal according to the specifications.
 
         This creates only symmetric unthresholded RPs.
         """
         reshaped_signal = self.embedded_signal.reshape(self.num_data_points, -1)
 
-        for i in range(self.num_data_points):
-            dist = self.metric(reshaped_signal[i, :], reshaped_signal)
-            self._recurrence_plot[:, i] = dist
+        self._recurrence_plot = cdist(reshaped_signal, reshaped_signal, metric=self.metric)
 
         self._recurrence_plot = self._recurrence_plot[::-1, :]
 
@@ -53,26 +54,19 @@ if __name__ == "__main__":
     from embeddings.lag_emebedding import LagEmbedding
     from embeddings.utils.fnn import fnn
     from embeddings.utils.mutual_information import mutual_information
-    from metrics import cosine_dist
     from signals.artificial_signals import Sinusoid
 
-    sinusoid = Sinusoid(frequency=1, sampling_rate=200, sec=5, noise_rate=0.1)
+    sinusoid = Sinusoid(frequency=1, sampling_rate=200, sec=5, noise_rate=0.5)
     sinusoid_signal = sinusoid.generate()
     sinusoid.show()
     lag = mutual_information(signal=sinusoid_signal)
     dim = fnn(signal=sinusoid_signal, lag=lag)
     embedding = LagEmbedding(dim=dim, lag=lag)
-
-    rp = RecurrencePlot(signal=sinusoid_signal, embedding=embedding, metric=cosine_dist)
+    print(f"Dim: {dim} | Lag: {lag}")
+    rp = RecurrencePlot(signal=sinusoid_signal, embedding=embedding, metric="euclidean")
 
     start = time.time()
     rp.generate()
-    end = time.time()
-    print("Elapsed = %s" % (end - start))
-    rp.show()
-
-    start = time.time()
-    rp.generate(normalize=False)
     end = time.time()
     print("Elapsed = %s" % (end - start))
     rp.show()

41230e3eefd0f53872f0feb36a0e0d7436274276
carstengieshoff <carsten.gieshoff@rwth-aachen.de>
2022-02-26 15:38:14 +0100

    Refactor variable names in byest

--- signal_processing/byest.py
+++ signal_processing/byest.py
@@ -2,14 +2,14 @@ import numpy as np
 from scipy.linalg import toeplitz
 
 
-def byest(Y: np.array, R: np.array, Fs: int, leadAF: int = 1, nbvec: int = 5) -> np.array:
+def byest(Y: np.array, r_peaks: np.array, Fs: int, leadAF: int = 1, nbvec: int = 5) -> np.array:
     """Byesian estimation of AF signal.
 
 
     Args:
         Y: n-leads input signal (signals organized on rows, samples on columns
          (Y = (n x N) matrix))
-        R: R-wave time instant series (vector containing time instants of all R
+        r_peaks: R-wave time instant series (vector containing time instants of all R
         waves in the signal)
         Fs: sampling frequency
         leadAF: number of the lead we want to extract the atrial activity from
@@ -21,12 +21,12 @@ def byest(Y: np.array, R: np.array, Fs: int, leadAF: int = 1, nbvec: int = 5) ->
     """
     nblead = min(Y.shape)
 
-    RR = R[1:] - R[:-1]
+    r_peak_dist = r_peaks[1:] - r_peaks[:-1]
 
     indmin = np.floor(0.04 * Fs).astype(int)
-    indmax = np.floor(RR.min()).astype(int) - indmin - 1
+    indmax = np.floor(r_peak_dist.min()).astype(int) - indmin - 1
 
-    R = R[R > indmin]
+    r_peaks = r_peaks[r_peaks > indmin]
 
     X = np.zeros(shape=(nblead, indmax + indmin + 1))
 
@@ -35,45 +35,47 @@ def byest(Y: np.array, R: np.array, Fs: int, leadAF: int = 1, nbvec: int = 5) ->
 
     len_y = max(Y.shape)
 
-    for k in range(R.shape[0]):
-        peak = R[k]
-        if R[k] + indmax < len_y:
+    for k in range(r_peaks.shape[0]):
+        peak = r_peaks[k]
+        if r_peaks[k] + indmax < len_y:
             X += Y[:, peak - indmin : peak + indmax + 1]
         else:
             remainder = Y[:, peak - indmin :]
             fill = np.zeros(shape=(nblead, X.shape[1] - remainder.shape[1]))
             X += np.vstack([remainder.T, fill.T]).T
 
-    X = X / R.shape[0]
-    U, S, V = np.linalg.svd(X)  # unused
+    X = X / r_peaks.shape[0]
 
-    # M = V[:nbvec,:].T #unused
     X = X.T
 
+    # Original
     Un = np.ones(shape=(indmax + indmin + 1, 1)) / np.sqrt(indmax + indmin + 1)
     A = np.vstack([Un.T, np.arange(1, indmax + indmin + 1 + 1).reshape(-1, 1).T]).squeeze().T
-
     U1, S, V = np.linalg.svd((np.eye(indmax + indmin + 1) - A @ np.linalg.pinv(A)) @ X)
-    M1 = np.vstack([U1[:, :nbvec].T, A.T]).T
-    K = M1
+    H = np.vstack([U1[:, :nbvec].T, A.T]).T
+
+    # Option 2:
+    # A = Un
+    # U1, S, V = np.linalg.svd((np.eye(indmax + indmin + 1) - A @ np.linalg.pinv(A)) @ X)
+    # K_a = U1[:, :nbvec]
+    # H = np.vstack([U1[:, :nbvec].T, np.arange(1, indmax + indmin + 1 + 1).reshape(-1, 1).T]).T
 
     # LS estimated noise
-    IK = np.linalg.pinv(K)
+    IK = np.linalg.pinv(H)
     ri = np.empty(shape=(indmax + indmin + 1,))
-    L = R.shape[0] - 1
+    L = r_peaks.shape[0] - 1
     for ll in range(L):
-        SI = Y[:, R[ll] - indmin : R[ll] + indmax + 1].T
-        SIest = K @ IK @ SI
-        AF = SI - SIest
+        x = Y[:, r_peaks[ll] - indmin : r_peaks[ll] + indmax + 1].T
+        AF = (np.eye(indmin + indmax + 1) - H @ IK) @ x
         if ll == 0:
             ri = AF[:, leadAF]
         else:
             ri = np.vstack([ri.T, AF[:, leadAF].T]).T
 
-    rb = np.concatenate([Y[leadAF, : R[0] - indmin - 1 + 1], ri[:, 0]])
-    for k in range(1, L):
-        rb = np.concatenate([rb, Y[leadAF, R[k - 1] + indmax + 1 : R[k] - indmin - 1 + 1], ri[:, k]])
-    rb = np.concatenate([rb, Y[leadAF, rb.shape[0] + 1 - 1 :]])
+    # rb = np.concatenate([Y[leadAF, : r_peaks[0] - indmin - 1 + 1], ri[:, 0]])
+    # for k in range(1, L):
+    #    rb = np.concatenate([rb, Y[leadAF, r_peaks[k - 1] + indmax + 1: r_peaks[k] - indmin - 1 + 1], ri[:, k]])
+    # rb = np.concatenate([rb, Y[leadAF, rb.shape[0] + 1 - 1 :]])
 
     # covariance matrix estimation
     c = np.zeros(shape=((indmax + indmin) * 2 + 1,))
@@ -83,34 +85,34 @@ def byest(Y: np.array, R: np.array, Fs: int, leadAF: int = 1, nbvec: int = 5) ->
         c = c + np.correlate(a=ri[:, k], v=ri[:, k], mode="full") / ri.shape[0]
     c = c / LL
 
-    IC = np.linalg.inv(toeplitz(c=c[indmax + indmin : 2 * (indmax + indmin) + 1 + 1] / (indmax + indmin + 1)))
+    Cb = toeplitz(c=c[indmax + indmin : 2 * (indmax + indmin) + 1 + 1] / (indmax + indmin + 1))
+    ICb = np.linalg.inv(Cb)
 
     # BLUE estimation
-    IK = np.linalg.inv(K.T @ IC @ K) @ K.T @ IC
+    IK = np.linalg.inv(H.T @ ICb @ H) @ H.T @ ICb
     for ll in range(LL):
-        SI = Y[:, R[ll] - indmin : R[ll] + indmax + 1].T
-        SIest = K @ IK @ SI
-        AF = SI - SIest
+        x = Y[:, r_peaks[ll] - indmin : r_peaks[ll] + indmax + 1].T
+        AF = (np.eye(indmin + indmax + 1) - H @ IK) @ x
         if ll == 0:
             rib = AF[:, leadAF]
         else:
             rib = np.vstack([rib.T, AF[:, leadAF].T]).T
 
     # reconstitution of AF signal
-    re = np.concatenate([Y[leadAF, 0 : R[0] - indmin - 1 + 1].T, rib[:, 0]])
+    re = np.concatenate([Y[leadAF, 0 : r_peaks[0] - indmin - 1 + 1].T, rib[:, 0]])
     for k in range(1, L):
-        re = np.concatenate([re, Y[leadAF, R[k - 1] + indmax + 1 : R[k] - indmin - 1 + 1].T, rib[:, k]])
+        re = np.concatenate([re, Y[leadAF, r_peaks[k - 1] + indmax + 1 : r_peaks[k] - indmin - 1 + 1].T, rib[:, k]])
     re = np.concatenate([re.T, Y[leadAF, re.shape[0] :]])
 
     # reconstitution with continuity at connection points
-    re2 = np.concatenate([Y[leadAF, : R[0] - indmin - 1 + 1].T, rib[:, 0]])
+    re2 = np.concatenate([Y[leadAF, : r_peaks[0] - indmin - 1 + 1].T, rib[:, 0]])
     for k in range(1, L):
         c1 = rib[indmin + indmax, k - 1]
         c2 = rib[0, k]
-        N = R[k] - R[k - 1] - indmin - indmax + 1
-        bet = (Y[leadAF, R[k] - indmin] - Y[leadAF, R[k - 1] + indmax] - c2 + c1) / (N - 1)
-        alp = Y[leadAF, R[k - 1] + indmax] - c1 - bet
-        vec = Y[leadAF, R[k - 1] + indmax : R[k] - indmin + 1] - (alp + bet * np.arange(1, N + 1))
+        N = r_peaks[k] - r_peaks[k - 1] - indmin - indmax + 1
+        bet = (Y[leadAF, r_peaks[k] - indmin] - Y[leadAF, r_peaks[k - 1] + indmax] - c2 + c1) / (N - 1)
+        alp = Y[leadAF, r_peaks[k - 1] + indmax] - c1 - bet
+        vec = Y[leadAF, r_peaks[k - 1] + indmax : r_peaks[k] - indmin + 1] - (alp + bet * np.arange(1, N + 1))
         re2 = np.concatenate([re2, vec[1:-1].T, rib[:, k]])
 
     re2 = np.concatenate([re2, Y[leadAF, re2.shape[0] :].T]).T

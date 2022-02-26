de8132e6af772b00c4e44d2c53b901b7958d4b92
carstengieshoff <carsten.gieshoff@rwth-aachen.de>
2022-02-26 16:10:19 +0100

    Refactor variable names in byest

--- signal_processing/byest.py
+++ signal_processing/byest.py
@@ -2,8 +2,16 @@ import numpy as np
 from scipy.linalg import toeplitz
 
 
-def byest(Y: np.array, r_peaks: np.array, Fs: int, leadAF: int = 1, nbvec: int = 5) -> np.array:
-    """Byesian estimation of AF signal.
+def byest(
+    Y: np.array,
+    r_peaks: np.array,
+    Fs: int,
+    leadAF: int = 1,
+    nbvec: int = 5,
+    continuous_connections: bool = True,
+    same_length: bool = True,
+) -> np.array:
+    """Bayesian estimation of AF signal.
 
 
     Args:
@@ -15,6 +23,8 @@ def byest(Y: np.array, r_peaks: np.array, Fs: int, leadAF: int = 1, nbvec: int =
         leadAF: number of the lead we want to extract the atrial activity from
          (generally V1)
         nbvec: number of principal components considered
+        continuous_connections: Indicator whether to return a continuous signal. This defaults to True.
+        same_length: Indicator whether to enforce returning a signal of same length.
 
     Returns:
         Extracted atrial activity signal
@@ -25,10 +35,11 @@ def byest(Y: np.array, r_peaks: np.array, Fs: int, leadAF: int = 1, nbvec: int =
 
     indmin = np.floor(0.04 * Fs).astype(int)
     indmax = np.floor(r_peak_dist.min()).astype(int) - indmin - 1
+    window_length = indmin + indmax + 1
 
     r_peaks = r_peaks[r_peaks > indmin]
 
-    X = np.zeros(shape=(nblead, indmax + indmin + 1))
+    X = np.zeros(shape=(nblead, window_length))
 
     if Y.shape[0] > Y.shape[1]:
         Y = Y.T
@@ -98,25 +109,24 @@ def byest(Y: np.array, r_peaks: np.array, Fs: int, leadAF: int = 1, nbvec: int =
         else:
             rib = np.vstack([rib.T, AF[:, leadAF].T]).T
 
-    # reconstitution of AF signal
-    re = np.concatenate([Y[leadAF, 0 : r_peaks[0] - indmin - 1 + 1].T, rib[:, 0]])
-    for k in range(1, L):
-        re = np.concatenate([re, Y[leadAF, r_peaks[k - 1] + indmax + 1 : r_peaks[k] - indmin - 1 + 1].T, rib[:, k]])
-    re = np.concatenate([re.T, Y[leadAF, re.shape[0] :]])
+    re = np.concatenate([Y[leadAF, : r_peaks[0] - indmin - 1 + 1].T, rib[:, 0]])
+    if continuous_connections:
+        # reconstitution with continuity at connection points
+        for k in range(1, L):
+            c1 = rib[indmin + indmax, k - 1]
+            c2 = rib[0, k]
+            N = r_peaks[k] - r_peaks[k - 1] - indmin - indmax + 1
+            bet = (Y[leadAF, r_peaks[k] - indmin] - Y[leadAF, r_peaks[k - 1] + indmax] - c2 + c1) / (N - 1)
+            alp = Y[leadAF, r_peaks[k - 1] + indmax] - c1 - bet
+            vec = Y[leadAF, r_peaks[k - 1] + indmax : r_peaks[k] - indmin + 1] - (alp + bet * np.arange(1, N + 1))
+            re = np.concatenate([re, vec[1:-1].T, rib[:, k]])
 
-    # reconstitution with continuity at connection points
-    re2 = np.concatenate([Y[leadAF, : r_peaks[0] - indmin - 1 + 1].T, rib[:, 0]])
-    for k in range(1, L):
-        c1 = rib[indmin + indmax, k - 1]
-        c2 = rib[0, k]
-        N = r_peaks[k] - r_peaks[k - 1] - indmin - indmax + 1
-        bet = (Y[leadAF, r_peaks[k] - indmin] - Y[leadAF, r_peaks[k - 1] + indmax] - c2 + c1) / (N - 1)
-        alp = Y[leadAF, r_peaks[k - 1] + indmax] - c1 - bet
-        vec = Y[leadAF, r_peaks[k - 1] + indmax : r_peaks[k] - indmin + 1] - (alp + bet * np.arange(1, N + 1))
-        re2 = np.concatenate([re2, vec[1:-1].T, rib[:, k]])
-
-    re2 = np.concatenate([re2, Y[leadAF, re2.shape[0] :].T]).T
-    return re2
+    else:
+        # reconstitution of AF signal
+        for k in range(1, L):
+            re = np.concatenate([re, Y[leadAF, r_peaks[k - 1] + indmax + 1 : r_peaks[k] - indmin - 1 + 1].T, rib[:, k]])
+    re = np.concatenate([re, Y[leadAF, re.shape[0] :].T]).T
+    return re
 
 
 if __name__ == "__main__":
@@ -131,13 +141,15 @@ if __name__ == "__main__":
     for lead in range(12):
         data_af[:, lead] = byest(data_centered, qrs_locs, fs, lead)
 
-    fig, ax = plt.subplots(12, 1, figsize=(10, 40))
+    fig, ax = plt.subplots(12, 2, figsize=(10, 40))
 
     for i in range(12):
-        ax[i].plot(data_centered[:, i], label="original")
-        ax[i].plot(data_af[:, i], label="corrected")
-        ax[i].scatter(qrs_locs - 2, data_centered[qrs_locs - 2, i], marker="o", color="red")
-        ax[i].set_title(f"lead_{i + 1}")
-        ax[i].legend()
+        ax[i, 0].plot(data_centered[:, i], label="original")
+        ax[i, 0].plot(data_af[:, i], label="corrected")
+        ax[i, 0].scatter(qrs_locs - 2, data_centered[qrs_locs - 2, i], marker="o", color="red")
+        ax[i, 0].set_title(f"lead_{i + 1}")
+        ax[i, 0].legend()
+        ax[i, 1].plot(data_centered[:, i] - data_af[:, i])
+        ax[i, 1].set_title("denoised")
 
     plt.show()

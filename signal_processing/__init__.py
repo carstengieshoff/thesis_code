532166c6f92bae4c7a3824d5735c093c00aa62b6
carstengieshoff <carsten.gieshoff@rwth-aachen.de>
2022-03-03 09:55:03 +0100

    Add functionality for splitting signals into windows of equal length

--- signal_processing/__init__.py
+++ signal_processing/__init__.py
@@ -2,5 +2,6 @@ from signal_processing.detqrs import detqrs3
 from signal_processing.PCA import PCA
 from signal_processing.QRS_detection import QRSEstimator
 from signal_processing.r_peak_detection import get_r_peaks
+from signal_processing.signal_splitting import get_rr_intervals, split_signal
 
-__all__ = ["PCA", "detqrs3", "QRSEstimator", "get_r_peaks"]
+__all__ = ["PCA", "detqrs3", "QRSEstimator", "get_r_peaks", "split_signal", "get_rr_intervals"]

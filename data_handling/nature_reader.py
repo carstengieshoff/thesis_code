2c775916f764f85f6ef6ca8172298bbb64b03d90
carstengieshoff <carsten.gieshoff@rwth-aachen.de>
2022-03-03 22:02:12 +0100

    Adapt nature reader to include numerical representations of categories

--- data_handling/nature_reader.py
+++ data_handling/nature_reader.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Any, Dict, List, Literal, Union
+from typing import Any, Dict, List, Literal, Tuple, Union
 
 import numpy as np
 import pandas as pd
@@ -14,13 +14,15 @@ class NatureReader(DataReader):
     def __init__(self, path: Union[str, Path], use: DataType = "noisy"):
         super().__init__(path=path)
         self._use = use
-        self._info = self._read_info()
+        self._info, self.label_to_int, self.int_to_label = self._read_info()
 
     def get_dataset(self) -> List[DataPoint]:
         ds: List[DataPoint] = []
 
         for ref in tqdm(self._info.keys(), total=len(self._info)):
-            ds.append(DataPoint(self.get_data_from_reference(ref), self.get_label_from_reference(ref)))
+            ds.append(
+                DataPoint(self.get_data_from_reference(ref), self.label_to_int[self.get_label_from_reference(ref)])
+            )
 
         return ds
 
@@ -39,11 +41,15 @@ class NatureReader(DataReader):
         label: str = self._info[ref]["Rhythm"]
         return label
 
-    def _read_info(self) -> Dict[str, Dict[str, Any]]:
+    def _read_info(self) -> Tuple[Dict[str, Dict[str, Any]], Dict[str, int], Dict[int, str]]:
         info_df = pd.read_excel(self._path / "Diagnostics.xlsx")
         info_df.set_index("FileName", inplace=True)
         info: Dict[str, Dict[str, Any]] = info_df.T.to_dict()
-        return info
+
+        unique_labels = set(info[ref]["Rhythm"] for ref in info.keys())
+        label_to_int = dict(zip(unique_labels, list(range(len(unique_labels)))))
+        int_to_label = dict(zip(label_to_int.values(), label_to_int.keys()))
+        return info, label_to_int, int_to_label
 
     def _check_ref_exists(self, ref: str) -> None:
         if ref not in self._info.keys():

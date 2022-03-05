d49c56a83ff1f9f20e7d19988c93493c361ff8e5
carstengieshoff <carsten.gieshoff@rwth-aachen.de>
2022-03-05 12:28:37 +0100

    Add pipeline functionality

--- signal_processing/pipeline.py
+++ signal_processing/pipeline.py
@@ -5,6 +5,8 @@ from tqdm import tqdm
 
 from data_handling.data_reader import DataPoint
 from embeddings.utils import fnn, mutual_information
+from recurrence_plots import RecurrencePlotCalculator
+from signal_processing import QRSEstimator, get_r_peaks
 from visualizations import plot_hist2d
 
 
@@ -13,6 +15,43 @@ class SignalProcessingPipeline:
         self.dataset = dataset.copy()
         self.Fs = Fs
         self.description = ""
+
+    def calc_rps(
+        self, rp_calculator: RecurrencePlotCalculator, normalize: bool = True, *args: Any, **kwargs: Any
+    ) -> None:
+        self.description += "_" + str(rp_calculator)
+        ds_new: List[DataPoint] = []
+        for x, y in tqdm(self.dataset, total=len(self.dataset)):
+            rp = rp_calculator.generate(signal=x)
+            if normalize:
+                rp.normalize()
+            ds_new.append(DataPoint(np.expand_dims(rp.get_rp(*args, **kwargs), axis=0), y))
+
+        self.dataset = ds_new
+
+    def remove_qrs(self, qrs_estimator: QRSEstimator) -> None:
+
+        ds_new: List[DataPoint] = []
+        for x, y in tqdm(self.dataset, total=len(self.dataset)):
+            try:
+                qrs_locs = get_r_peaks(x[:, 0], self.Fs)
+                _, b = qrs_estimator(x, qrs_locs)
+                # for window in range(b.shape[1]):
+                window = 0
+                ds_new.append(DataPoint(b[:, window, :].T, y))
+            except IndexError:
+                pass
+
+        self.dataset = ds_new
+
+    def filter_signal(self) -> None:
+        pass
+
+    def log_message(self) -> None:
+        pass
+
+    def save_dataset(self) -> None:
+        pass
 
     def get_ebedding_info(self, num_samples: int = 0, *args: Any, **kwargs: Any) -> None:
         # TODO: Add option to plot this for any group of labels
@@ -34,7 +73,7 @@ class SignalProcessingPipeline:
         lags = np.array(lags, dtype="uint8")
         dims = np.array(dims, dtype="uint8")
 
-        plot_hist2d(lags, dims, *args, **kwargs)
+        plot_hist2d(lags, dims, ylabel="lag", xlabel="dims", figsize=(20, 20))
 
     def _check_all_signals_same_shape(self) -> bool:
         """Check if all signals in `dataset` have the same length."""
--- visualizations/hist.py
+++ visualizations/hist.py
@@ -1,11 +1,12 @@
-from typing import Any
+from typing import Any, Optional
 
 import numpy as np
 from matplotlib import pyplot as plt
-from matplotlib.axes import Axes
 
 
-def plot_hist2d(a: np.array, b: np.array, *args: Any, **kwargs: Any) -> Axes:
+def plot_hist2d(
+    a: np.array, b: np.array, xlabel: Optional[str] = None, ylabel: Optional[str] = None, *args: Any, **kwargs: Any
+) -> None:
     """Plot 2D histrogram.
 
     Args:
@@ -13,16 +14,29 @@ def plot_hist2d(a: np.array, b: np.array, *args: Any, **kwargs: Any) -> Axes:
         b: np.array of shape (len_b,)
     """
     # Todo: Add margin distrubutions
-    # TODO: Log argmax(Hist)
     a_bins = np.arange(-1, max(a) + 1) + 0.5
     b_bins = np.arange(-1, max(b) + 1) + 0.5
     hist, xedges, yedges = np.histogram2d(a, b, bins=(a_bins, b_bins))
 
-    fig, ax = plt.subplots(1, 1, *args, **kwargs)
+    fig, ax = plt.subplots(*args, **kwargs)
     pos = ax.imshow(hist)
-    ax.set_ylabel("lag")
-    ax.set_xlabel("dim")
+    if ylabel:
+        ax.set_ylabel(ylabel)
+    if xlabel:
+        ax.set_xlabel(xlabel)
     ax.set_yticks((xedges[:-1] + xedges[1:]) / 2)
     ax.set_xticks((yedges[:-1] + yedges[1:]) / 2)
     fig.colorbar(pos, ax=ax)
     plt.show()
+
+    id_x, id_y = (hist == hist.max()).nonzero()
+    id_x, id_y = id_x.item(), id_y.item()
+    print(f"Max at: ({id_x}, {id_y})")
+
+
+if __name__ == "__main__":
+
+    a = np.random.randint(0, 10, 100)
+    b = np.random.randint(0, 10, 100)
+
+    plot_hist2d(a=a, b=b)

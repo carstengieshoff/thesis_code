5616b8d076756a21ee33bcd7d08daa14719887a8
carstengieshoff <carsten.gieshoff@rwth-aachen.de>
2022-03-10 14:19:43 +0100

    Add UMDataReader

--- data_handling/__init__.py
+++ data_handling/__init__.py
@@ -2,5 +2,6 @@ from data_handling.data_reader import DataReader
 from data_handling.dataset import Dataset
 from data_handling.nature_reader import NatureReader, NatureReaderRestricted
 from data_handling.physionet_reader import PhysioNetReader
+from data_handling.um_data_reader import UMDataReader
 
-__all__ = ["DataReader", "PhysioNetReader", "NatureReader", "Dataset", "NatureReaderRestricted"]
+__all__ = ["DataReader", "PhysioNetReader", "NatureReader", "Dataset", "NatureReaderRestricted", "UMDataReader"]

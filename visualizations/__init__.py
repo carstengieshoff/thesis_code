2ae05bf4a501a6e3eba2875ec076d6794b5db497
carstengieshoff <carsten.gieshoff@rwth-aachen.de>
2022-03-16 10:29:04 +0100

    Add plot_filter to visualizations init

--- visualizations/__init__.py
+++ visualizations/__init__.py
@@ -1,6 +1,7 @@
 from visualizations.plot_fft import plot_fft
+from visualizations.plot_filter import plot_filter
 from visualizations.plot_hist import plot_hist2d
 from visualizations.plot_rp import plot_rp
 from visualizations.plot_spectral_envelope import plot_spectral_envelope
 
-__all__ = ["plot_hist2d", "plot_rp", "plot_fft", "plot_spectral_envelope"]
+__all__ = ["plot_hist2d", "plot_rp", "plot_fft", "plot_spectral_envelope", "plot_filter"]

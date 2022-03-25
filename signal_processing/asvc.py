ed70dfa1ffeae92adeec6d3e2fc8c07e63f89a02
carstengieshoff <carsten.gieshoff@rwth-aachen.de>
2022-03-25 09:50:58 +0100

    Refactor ASVC

--- signal_processing/asvc.py
+++ signal_processing/asvc.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Any, Optional
+from typing import Any, List, Optional, Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -13,6 +13,27 @@ logging.basicConfig(level=logging.INFO)
 
 
 class ASVCancellator:
+    def __init__(
+        self,
+        with_shift: bool = True,
+        use_clustering: bool = False,
+        min_cluster_size: Optional[int] = None,
+        smooth_template: Optional[int] = None,
+        pos_neg_fit: bool = False,
+        smooth_transitions: bool = True,
+        P: int = 40,
+        M: int = 20,
+    ):
+
+        self.with_shift = with_shift
+        self.use_clustering = use_clustering
+        self.min_cluster_size = min_cluster_size
+        self.smooth_template = smooth_template
+        self.pos_neg_fit = pos_neg_fit
+        self.smooth_transitions = smooth_transitions
+        self.P = P
+        self.M = M
+
     def reconstruct(self, *args: Any, **kwargs: Any) -> np.array:
         return self.__call__(*args, **kwargs)
 
@@ -21,15 +42,9 @@ class ASVCancellator:
         original_signal: np.array,
         r_peaks: np.array,
         verbose: bool = False,
-        fit: str = "normal",
-        P: int = 40,
-        M: int = 20,
-        use_clustering: bool = False,
-        min_cluster_size: Optional[int] = None,
-        plot: bool = True,
+        plot_all: bool = True,
         savefig: bool = False,
-        pos_neg_fit: bool = False,
-        smooth_template: Optional[int] = None,
+        plot_single_windows: Optional[List[Tuple[int, int]]] = None,
         *args: Any,
         **kwargs: Any,
     ) -> np.array:
@@ -55,37 +70,32 @@ class ASVCancellator:
         )
 
         # Windowing
-        X = split_signal(signal=signal_padded, r_peaks=r_peaks_shifted, front=front, back=back)
+        rr_windows = split_signal(signal=signal_padded, r_peaks=r_peaks_shifted, front=front, back=back)
 
         # Create Template (lead by lead)
         # Find subset of windows to use (e.g. by similarity, neighbours, clustering)
-        min_cluster_size = int(X.shape[1] / 4) if min_cluster_size is None else min_cluster_size
+        min_cluster_size = int(rr_windows.shape[1] / 4) if self.min_cluster_size is None else self.min_cluster_size
         template, cluster_labels = self._get_template(
-            X, plot_templates=verbose, use_clustering=use_clustering, min_cluster_size=min_cluster_size
+            rr_windows, plot_templates=verbose, use_clustering=self.use_clustering, min_cluster_size=min_cluster_size
         )
 
-        if smooth_template is not None:
-            h = np.ones(smooth_template) / smooth_template
+        if self.smooth_template is not None:
+            h = np.ones(self.smooth_template) / self.smooth_template
             template = filtfilt(h, 1, template, axis=2)
 
         # Fit template to window
-        # Fit by amplitude
-        if fit == "normal":
-            template_fitted = self._fit_template_to_windows_lstsq(X, template, verbose=verbose)
-        elif fit == "shifted":
-            template_fitted = self._fit_template_to_windows_shift_lstsq(
-                X, template, verbose=verbose, pos_neg_fit=pos_neg_fit
-            )
-        else:
-            raise RuntimeError(f"Unrecognized option for `fit`. Expected one of `normal`, `shifted`: got {fit}")
+        template_fitted = self._fit_template_to_windows(
+            rr_windows, template, verbose=verbose, pos_neg_fit=self.pos_neg_fit
+        )
 
         # fit transitions
         aa_signal, starts_ends = self._subtract_template(
-            windowed_signal=X, template=template_fitted, P=P, M=M, smooth_transitions=False
+            windowed_signal=rr_windows, template=template_fitted, P=self.P, M=self.M, smooth_transitions=False
         )
 
+        # Fit to original signal shape and smooth transitions
         aa_signal_reconstructed = self._reconstruct(
-            aa_signal, signal_padded, r_peaks_shifted, front, back, starts_ends, M=M
+            aa_signal, signal_padded, r_peaks_shifted, front, back, starts_ends, M=self.M
         )
 
         if pad_front > 0:
@@ -95,10 +105,8 @@ class ASVCancellator:
             aa_signal_reconstructed = aa_signal_reconstructed[:-pad_back]
 
         # Evaluate (optionally)
-
-        if verbose or plot:
-
-            self._plot(
+        if plot_all:
+            self._plot_all(
                 original_signal,
                 aa_signal_reconstructed,
                 r_peaks,
@@ -109,9 +117,44 @@ class ASVCancellator:
                 savefig=savefig,
             )
 
+        if plot_single_windows is not None:
+            for lead, window in plot_single_windows:
+                self._plot_window(
+                    lead=lead,
+                    window=window,
+                    windowed_signal=rr_windows,
+                    template=template,
+                    template_fitted=template_fitted,
+                )
+
         return aa_signal_reconstructed
 
-    def _plot(
+    def _plot_window(
+        self, lead: int, window: int, windowed_signal: np.array, template: np.array, template_fitted: np.array
+    ) -> None:
+
+        lead -= 1
+        window -= 1
+
+        plt.figure(figsize=(15, 8))
+        plt.plot(range(len(template[lead, window, :])), template[lead, window, :], label="Template")
+        plt.plot(range(len(windowed_signal[lead, window, :])), windowed_signal[lead, window, :], label="Original data")
+        plt.plot(
+            range(len(template_fitted[lead, window, :])), template_fitted[lead, window, :], label="Fitted Template (VA)"
+        )
+        plt.plot(
+            range(len(template_fitted[lead, window, :])),
+            windowed_signal[lead, window, :] - template_fitted[lead, window, :],
+            color="red",
+            label="Diff (AA)",
+        )
+        plt.legend()
+        plt.grid()
+        plt.title(f"Lead {lead+1}, window {window+1} | {str(self)}", fontsize="xx-large")
+        plt.savefig(f"./{str(self)}.png")
+        plt.show()
+
+    def _plot_all(
         self,
         original_signal: np.array,
         transformed_signal: np.array,
@@ -125,24 +168,24 @@ class ASVCancellator:
         signal_len, n_leads = original_signal.shape
 
         fig, ax = plt.subplots(n_leads, 4, figsize=(50, 60))
-        plt.title("QRST-cancellation using Adaptive Singluar Value Cancellation", fontsize=30)
+        plt.title("QRST-cancellation using" + str(self), fontsize="x-large")
         if n_leads > 1:
             for lead in range(n_leads):
                 ax[lead, 0].plot(original_signal[:, lead], label="original")
                 ax[lead, 0].scatter(r_peaks, original_signal[r_peaks, lead], marker="o", color="red", label="r-peaks")
-                ax[lead, 0].set_title(f"lead_{lead + 1} Original ECG")
+                ax[lead, 0].set_title(f"lead_{lead + 1} Original ECG", fontsize="large")
                 ax[lead, 0].legend()
 
                 ax[lead, 1].plot(transformed_signal[:, lead])
-                ax[lead, 1].set_title("Estimated AA")
+                ax[lead, 1].set_title("Estimated AA", fontsize="large")
 
                 ax[lead, 2].plot(
                     original_signal[:, lead] - transformed_signal[:, lead],
                 )
-                ax[lead, 2].set_title("Estimated VA (orig- Estimated AA)")
+                ax[lead, 2].set_title("Estimated VA (orig- Estimated AA)", fontsize="large")
 
                 ax[lead, 3].plot(template[lead, :, :].T)
-                ax[lead, 3].set_title(f"lead_{lead+1}-templates")
+                ax[lead, 3].set_title(f"lead_{lead+1}-templates", fontsize="large")
 
                 for w, peak in enumerate(r_peaks):
                     for j in range(3):
@@ -166,15 +209,15 @@ class ASVCancellator:
             ax[0].legend()
 
             ax[1].plot(transformed_signal)
-            ax[1].set_title("AA")
+            ax[1].set_title("AA", fontsize="large")
 
             ax[2].plot(
                 original_signal - transformed_signal,
             )
-            ax[2].set_title("VA (orig-AA)")
+            ax[2].set_title("VA (orig-AA)", fontsize="large")
 
             ax[3].plot(template[0, 0, :])
-            ax[3].set_title("lead-template")
+            ax[3].set_title("lead-template", fontsize="large")
 
             for j in range(3):
                 for w, peak in enumerate(r_peaks):
@@ -228,82 +271,60 @@ class ASVCancellator:
             labels = model.labels_
         return labels
 
-    def _fit_template_to_windows_lstsq(
-        self, windowed_signal: np.array, template: np.array, verbose: bool = False
-    ) -> np.array:
-        assert windowed_signal.shape == template.shape
-        n_leads, n_windows, window_size = windowed_signal.shape
-
-        coeffs = np.zeros(shape=(n_leads, n_windows, 2))
-
-        for lead in range(n_leads):
-
-            design_matrix = np.stack([template[lead, 0, :], np.ones_like(template[lead, 0, :])]).T
-            lstq_results = np.linalg.lstsq(a=design_matrix, b=windowed_signal[lead, :, :].T)
-            coeffs[lead, :, :] = lstq_results[0].T
-
-            if verbose:
-                logging.info(
-                    f"Fitting lead {lead+1}: a,b = {lstq_results[0].mean(axis=1)}, residual = {lstq_results[1]}"
-                )
-
-        template_aligned = template * np.expand_dims(coeffs[:, :, 0], axis=2) + np.expand_dims(coeffs[:, :, 1], axis=2)
-
-        # N = 100
-        # weights = gaussian(2*N, std = 2*np.sqrt(N))
-        # template_aligned[:,:,:N] *=weights[:N]
-        # template_aligned[:, :, -N:] *= weights[-N:]
-
-        return template_aligned
-
-    def _fit_template_to_windows_shift_lstsq(
+    def _fit_template_to_windows(
         self, windowed_signal: np.array, template: np.array, verbose: bool = False, pos_neg_fit: bool = False
     ) -> np.array:
         assert windowed_signal.shape == template.shape
         n_leads, n_windows, window_size = windowed_signal.shape
 
-        template_shifted = template.copy()
+        templates_aligned = template.copy()
 
         for lead in range(n_leads):
             for window in range(n_windows):
-                corr = np.correlate(
-                    np.abs(windowed_signal[lead, window, :]), np.abs(template[lead, window, :]), mode="same"
-                )
-                shift = np.argmax(corr) - window_size // 2
-                template_shifted[lead, window, :] = np.roll(template_shifted[lead, window, :], shift=shift)
+                if self.with_shift:
+                    corr = np.correlate(
+                        np.abs(windowed_signal[lead, window, :]), np.abs(template[lead, window, :]), mode="same"
+                    )
+                    shift = np.argmax(corr) - window_size // 2
+                    templates_aligned[lead, window, :] = np.roll(templates_aligned[lead, window, :], shift=shift)
 
-                # Ensuring "np.roll" does not create artifacts; values rolled over from the oter end of the array are
-                # replaced more adequately by the last feasible value
-                if shift > 0:
-                    template_shifted[lead, window, :shift] = template_shifted[lead, window, shift]
-                elif shift < 0:
-                    template_shifted[lead, window, shift:] = template_shifted[lead, window, shift - 1]
-                else:
-                    pass
+                    # Ensuring "np.roll" does not create artifacts; values rolled over from the oter end of the array
+                    # are replaced more adequately by the last feasible value
+                    if shift > 0:
+                        templates_aligned[lead, window, :shift] = templates_aligned[lead, window, shift]
+                    elif shift < 0:
+                        templates_aligned[lead, window, shift:] = templates_aligned[lead, window, shift - 1]
+                    else:
+                        pass
 
                 if pos_neg_fit:
                     template_pos = np.where(
-                        template_shifted[lead, window, :] >= 0, template_shifted[lead, window, :], 0
+                        templates_aligned[lead, window, :] >= 0, templates_aligned[lead, window, :], 0
                     )
-                    template_neg = np.where(template_shifted[lead, window, :] < 0, template_shifted[lead, window, :], 0)
+                    template_neg = np.where(
+                        templates_aligned[lead, window, :] < 0, templates_aligned[lead, window, :], 0
+                    )
                     design_matrix = np.stack([template_pos, template_neg, np.ones_like(template[lead, window, :])]).T
                 else:
                     design_matrix = np.stack(
-                        [template_shifted[lead, window, :], np.ones_like(template[lead, window, :])]
+                        [templates_aligned[lead, window, :], np.ones_like(template[lead, window, :])]
                     ).T
 
                 lstq_results = np.linalg.lstsq(a=design_matrix, b=windowed_signal[lead, window, :].T)
-                template_shifted[lead, window, :] = np.dot(design_matrix, lstq_results[0])
+                templates_aligned[lead, window, :] = np.dot(design_matrix, lstq_results[0])
 
                 if verbose:
-                    logging.info(
-                        f"Fitting lead {lead+1}, window {window}:"
+                    info_msg = (
+                        f"Fitting lead {lead+1},"
+                        f" window {window},"
                         f" coeffs = {lstq_results[0]},"
                         f" residual = {lstq_results[1]},"
-                        f" shift {shift}"
                     )
+                    if self.with_shift:
+                        info_msg += f" shift {shift}"
+                    logging.info(info_msg)
 
-        return template_shifted
+        return templates_aligned
 
     def _subtract_template(
         self,
@@ -334,21 +355,6 @@ class ASVCancellator:
 
                 starts_ends[lead, window, :] = np.array([start, end])
 
-                # post-process
-                # if start >= 1:
-                #    M_ = min(M, start)
-                #    gaussian_window = gaussian(2 * M_, np.sqrt(M_))
-                #    ks = (aa_signal[lead, window, start - 1] - aa_signal[lead, window, start]) / 2
-                #    aa_signal[lead, window, start - M_ : start] -= ks * gaussian_window[:M_]
-                #    aa_signal[lead, window, start : start + M_] += ks * gaussian_window[M_:]
-        #
-        # if end < window_size - 1:
-        #    M_ = min(M, window_size - end - 1)
-        #    gaussian_window = gaussian(2 * M_, np.sqrt(M_))
-        #    ke = (aa_signal[lead, window, end] - aa_signal[lead, window, end + 1]) / 2
-        #    aa_signal[lead, window, end - M_ + 1 : end + 1] -= ke * gaussian_window[:M_]
-        #    aa_signal[lead, window, end + 1 : end + M_ + 1] += ke * gaussian_window[M_:]
-        #
         return aa_signal, starts_ends
 
     def _reconstruct(
@@ -382,26 +388,27 @@ class ASVCancellator:
             gap -= self._linreg(data=gap)
             reconstructed_signal[last_window_end_idx + 1 :, :] = gap
 
-        for window, peak in enumerate(r_peaks):
-            window_start = peak - front
-            window_end = peak + back + 1
+        if self.smooth_transitions:
+            for window, peak in enumerate(r_peaks):
+                window_start = peak - front
+                window_end = peak + back + 1
 
-            for lead in range(reconstructed_signal.shape[1]):
-                start = window_start + starts_ends[lead, window, 0]
-                end = window_start + starts_ends[lead, window, 1]
+                for lead in range(reconstructed_signal.shape[1]):
+                    start = window_start + starts_ends[lead, window, 0]
+                    end = window_start + starts_ends[lead, window, 1]
 
-                M_ = M
-                gaussian_window = gaussian(2 * M_, np.sqrt(2 * M_))
+                    M_ = M
+                    gaussian_window = gaussian(2 * M_, np.sqrt(2 * M_))
 
-                if start > M_:
-                    ks = (reconstructed_signal[start - 1, lead] - reconstructed_signal[start, lead]) / 2
-                    reconstructed_signal[start - M_ : start, lead] -= ks * gaussian_window[:M_]
-                    reconstructed_signal[start : start + M_, lead] += ks * gaussian_window[M_:]
+                    if start > M_:
+                        ks = (reconstructed_signal[start - 1, lead] - reconstructed_signal[start, lead]) / 2
+                        reconstructed_signal[start - M_ : start, lead] -= ks * gaussian_window[:M_]
+                        reconstructed_signal[start : start + M_, lead] += ks * gaussian_window[M_:]
 
-                if end < reconstructed_signal.shape[0] - M_:
-                    ke = (reconstructed_signal[end, lead] - reconstructed_signal[end + 1, lead]) / 2
-                    reconstructed_signal[end - M_ + 1 : end + 1, lead] -= ke * gaussian_window[:M_]
-                    reconstructed_signal[end + 1 : end + M_ + 1, lead] += ke * gaussian_window[M_:]
+                    if end < reconstructed_signal.shape[0] - M_:
+                        ke = (reconstructed_signal[end, lead] - reconstructed_signal[end + 1, lead]) / 2
+                        reconstructed_signal[end - M_ + 1 : end + 1, lead] -= ke * gaussian_window[:M_]
+                        reconstructed_signal[end + 1 : end + M_ + 1, lead] += ke * gaussian_window[M_:]
 
         return reconstructed_signal
 
@@ -416,6 +423,19 @@ class ASVCancellator:
 
         return fitted_data
 
+    def __str__(self) -> str:
+        string_repr: List[str] = []
+        if self.with_shift:
+            string_repr.append("Shift")
+        if self.use_clustering:
+            string_repr.append(" Clustering")
+        if self.pos_neg_fit:
+            string_repr.append(" Pos-Neg fit")
+
+        concatenator = "" if len(string_repr) == 0 else "with "
+
+        return "ASCV " + concatenator + ", ".join(string_repr)
+
 
 if __name__ == "__main__":
     from scipy.io import loadmat
@@ -428,18 +448,21 @@ if __name__ == "__main__":
     data_centered = data_centered - data_centered.mean(axis=0)
     fs = 1000
 
-    qrs_locs = detqrs3(data_centered[:, 9], fs)  # get_r_peaks(data_centered[:,0], fs)
-    asvc = ASVCancellator()
-
-    data_af = asvc(
-        original_signal=data_centered,
-        r_peaks=qrs_locs[1:],
-        verbose=True,
-        fit="shifted",
+    qrs_locs = detqrs3(data_centered[:, 0], fs)  # get_r_peaks(data_centered[:,0], fs)
+    asvc = ASVCancellator(
+        with_shift=False,
         P=40,
         M=20,
         use_clustering=False,
         pos_neg_fit=False,
-        smooth_template=100,
-        savefig=True,
+        smooth_template=None,
+    )
+
+    data_af = asvc(
+        original_signal=data_centered[:, :5],
+        r_peaks=qrs_locs[1:],
+        verbose=True,
+        savefig=False,
+        plot_all=False,
+        plot_single_windows=[(5, 10)],
     )

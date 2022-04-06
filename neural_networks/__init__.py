8e62baf4876a920795dce21ffd9d50623a4a73bf
carstengieshoff <carsten.gieshoff@rwth-aachen.de>
2022-04-06 14:37:02 +0200

    Add autoencoder class with adapted training and evaluation

--- neural_networks/__init__.py
+++ neural_networks/__init__.py
@@ -1,3 +1,4 @@
+from neural_networks.autoencoder import AutoEncoder
 from neural_networks.CNN import CNN
 
-__all__ = ["CNN"]
+__all__ = ["CNN", "AutoEncoder"]
---
+++ neural_networks/autoencoder.py
@@ -0,0 +1,109 @@
+from typing import Any, List, Optional
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from matplotlib import pyplot as plt
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+
+class AutoEncoder(nn.Module):  # type: ignore
+    def __init__(self, writer: Optional[SummaryWriter] = None) -> None:
+        super().__init__()
+        self.train_loss: List[float] = []
+        self.val_loss: List[float] = []
+        self.writer = writer
+
+    def forward(self, x: torch.tensor) -> torch.tensor:
+        return x
+
+    def train_nn(
+        self,
+        num_epochs: int,
+        trainloader: DataLoader,
+        validationloader: DataLoader,
+        optimizer: optim.Optimizer,
+        criterion: nn.Module,
+        device: torch.device,
+        show_every_n: int = 100,
+        plot_loss: bool = True,
+        *args: Any,
+        **kwargs: Any,
+    ) -> None:
+        """Train the nn."""
+        n_total_steps = len(trainloader)
+        running_loss = []
+        for epoch in range(num_epochs):
+
+            for i, data in enumerate(trainloader, 0):
+                inputs, labels = data
+                # labels = labels.to(device)
+                inputs = inputs.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward + backward + optimize
+                outputs = self.forward(inputs.float())
+                loss = criterion(outputs.to(device), inputs)
+
+                if loss.isnan().any():
+                    print(inputs, labels)
+                    raise RuntimeError("nans in loss")
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                # print statistics
+                running_loss.append(loss.item())
+                if i % show_every_n == (show_every_n - 1):
+                    self.train_loss.append(sum(running_loss) / len(running_loss))
+                    print(f"[{epoch + 1}, {i + 1:5d}] loss: {self.train_loss[-1]:.3f}")
+
+                    if self.writer is not None:
+                        if validationloader:
+                            self.val_loss.append(self.evaluate_nn(validationloader, device=device, criterion=criterion))
+                            self.writer.add_scalars(
+                                "Training",
+                                {"training loss": self.train_loss[-1], "validation loss": self.val_loss[-1]},
+                                epoch * n_total_steps + i,
+                            )
+                        else:
+                            self.writer.add_scalar("training loss", self.train_loss[-1], epoch * n_total_steps + i)
+
+                    running_loss = []
+
+        if plot_loss and self.writer is None:
+            self.plot_loss(*args, **kwargs)
+
+    def evaluate_nn(self, loader: DataLoader, device: torch.device, criterion: nn.Module) -> float:
+        running_loss = []
+
+        with torch.no_grad():
+            for data in loader:
+                images, _ = data
+                images = images.to(device)
+
+                outputs = self.forward(images.float())
+                loss = criterion(outputs.to(device), images)
+                running_loss.append(loss.item())
+
+        return sum(running_loss) / len(running_loss)
+
+    def plot_loss(self, *args: Any, **kwargs: Any) -> None:
+
+        plt.figure(*args, **kwargs)
+
+        ax = plt.subplot(111)
+        losses = np.array(self.train_loss)
+        losses_scaled = 100 * losses / losses.max()
+        ax.plot(losses_scaled, label="loss")
+        if self.val_loss:
+            ax.plot(self.val_loss, label="validation")
+
+        ax.legend()
+        plt.show()

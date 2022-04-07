0ccbaaf59c313df4e4cc21988819dac6f162a69f
carstengieshoff <carsten.gieshoff@rwth-aachen.de>
2022-04-07 21:04:57 +0200

    New R-peak-detetcion

--- signal_processing/r_peak_detection.py
+++ signal_processing/r_peak_detection.py
@@ -1,5 +1,8 @@
+import logging
+
 import neurokit2 as nk
 import numpy as np
+from ecgdetectors import Detectors
 
 
 def get_r_peaks(signal: np.array, sampling_rate: int, return_bools: bool = False) -> np.array:
@@ -13,6 +16,78 @@ def get_r_peaks(signal: np.array, sampling_rate: int, return_bools: bool = False
         return r_peaks_bool
     else:
         return r_peaks_idxs
+
+
+class RPeakDetector(Detectors):  # type: ignore
+    def __init__(
+        self,
+        sampling_frequency: float,
+        min_dist: float = 0.3,
+        max_dist: float = 1.2,
+        max_num: int = 20,
+        min_num: int = 4,
+    ) -> None:
+        super().__init__(sampling_frequency=sampling_frequency)
+        self.min_dist = int(min_dist * sampling_frequency)
+        self.max_dist = int(max_dist * sampling_frequency)
+        self.max_num = max_num
+        self.min_num = min_num
+
+        self.removals = 0
+
+    def detect(self, signal: np.array, correct_peaks: bool = True) -> np.array:
+        r_peaks = super().pan_tompkins_detector(signal)
+        r_peaks = np.asarray(r_peaks)
+
+        if correct_peaks:
+            r_peaks = self.correct(signal, r_peaks)
+
+        if len(r_peaks) < self.min_num:
+            self.removals += 1
+            logging.info(f"Too few r-peaks detected: {len(r_peaks)}, need at least {self.min_num}")
+            return None
+
+        if len(r_peaks) > self.max_num:
+            self.removals += 1
+            logging.info(f"Too may r-peaks detected: {len(r_peaks)}, can have at most {self.max_num}")
+            return None
+
+        rr_dist = r_peaks[1:] - r_peaks[:-1]
+        if rr_dist.min() < self.min_dist:
+            self.removals += 1
+            logging.info("Too short r-peak distance ")
+            return None
+
+        if rr_dist.max() > self.max_dist:
+            self.removals += 1
+            logging.info("Too long distance between r-peaks")
+            return None
+
+        return r_peaks
+
+    def correct(self, signal: np.array, peaks: np.array) -> np.array:
+        # from https://dsp.stackexchange.com/questions/58155/how-to-filter-ecg-and-detect-r-peaks
+
+        num_peak = peaks.shape[0]
+        peaks_corrected_list = list()
+        for index in range(num_peak):
+            i = peaks[index]
+            cnt = i
+            if cnt - 1 < 0:
+                break
+            if signal[cnt] < signal[cnt - 1]:
+                while signal[cnt] < signal[cnt - 1]:
+                    cnt -= 1
+                    if cnt < 0:
+                        break
+            elif signal[cnt] < signal[cnt + 1]:
+                while signal[cnt] < signal[cnt + 1]:
+                    cnt += 1
+                    if cnt < 0 or cnt == signal.shape[0] - 1:
+                        break
+            peaks_corrected_list.append(cnt)
+        peaks_corrected = np.asarray(peaks_corrected_list)
+        return peaks_corrected
 
 
 if __name__ == "__main__":

0ad3925d3e7b98098476ec71697457d00fc85842
carstengieshoff <carsten.gieshoff@rwth-aachen.de>
2022-04-07 11:14:58 +0200

    Add VAE to init

--- neural_networks/__init__.py
+++ neural_networks/__init__.py
@@ -1,4 +1,5 @@
 from neural_networks.autoencoder import AutoEncoder
 from neural_networks.CNN import CNN
+from neural_networks.VAE import VAE
 
-__all__ = ["CNN", "AutoEncoder"]
+__all__ = ["CNN", "AutoEncoder", "VAE"]

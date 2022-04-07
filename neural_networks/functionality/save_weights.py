4569f6bf08f7734fd4a8bec203a78c0f3d0400c9
carstengieshoff <carsten.gieshoff@rwth-aachen.de>
2022-04-07 10:30:47 +0200

    Adaptations to nn functionality

--- neural_networks/functionality/save_weights.py
+++ neural_networks/functionality/save_weights.py
@@ -11,10 +11,10 @@ def save_weights(
 
     global_name = "" if global_name is None else global_name + "_"
 
-    for layer in model.children():
-        if repr(layer).lower().startswith(layer_name):
+    for name, layer in model.named_children():
+        if name.startswith(layer_name):
             weight = layer.weight.detach()
             weight = weight.reshape(-1, 1, weight.shape[-2], weight.shape[-1])
 
             img_grid = make_grid(weight)
-            writer.add_image(global_name + repr(layer), img_grid)
+            writer.add_image(global_name + name, img_grid)

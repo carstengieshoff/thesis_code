31ce41f25cf3dc131090aaaf66376ec40d315be5
carstengieshoff <carsten.gieshoff@rwth-aachen.de>
2022-04-07 21:56:36 +0200

    New R-peak-detetcion

--- signal_processing/__init__.py
+++ signal_processing/__init__.py
@@ -6,7 +6,8 @@ from signal_processing.normalize import normalize
 from signal_processing.PCA import PCA
 from signal_processing.pipeline import SignalProcessingPipeline
 from signal_processing.qrs_cancellation import QRSEstimator
-from signal_processing.r_peak_detection import RPeakDetector, get_r_peaks
+from signal_processing.r_peak_detection import get_r_peaks
+from signal_processing.r_peak_detector import RPeakDetector
 
 __all__ = [
     "Byest",

5301bf56900932d6ded8de7da0572cca71d9ab2d
carstengieshoff <carsten.gieshoff@rwth-aachen.de>
2022-04-11 21:40:20 +0200

    Add scatter plot functionality

--- neural_networks/functionality/__init__.py
+++ neural_networks/functionality/__init__.py
@@ -1,5 +1,6 @@
 from neural_networks.functionality.get_activations import get_inp_activations, get_out_activations
 from neural_networks.functionality.get_opt_input_for_activation import get_opt_input_for_activation
+from neural_networks.functionality.plot_scatter_activations import plot_scatter
 from neural_networks.functionality.save_activation import save_activation
 from neural_networks.functionality.save_weights import save_weights
 
@@ -9,4 +10,5 @@ __all__ = [
     "get_out_activations",
     "get_inp_activations",
     "get_opt_input_for_activation",
+    "plot_scatter",
 ]

b96e64dba6ef535ac509b5108d58fa2247ddd24d
carstengieshoff <carsten.gieshoff@rwth-aachen.de>
2022-05-02 13:57:57 +0200

    Add Artificial AF Signal to init

--- signals/__init__.py
+++ signals/__init__.py
@@ -1,3 +1,3 @@
-from signals.artificial_signals import Chirp, Sinusoid
+from signals.artificial_signals import AF, Chirp, Sinusoid, af_type_a, af_type_b
 
-__all__ = ["Sinusoid", "Chirp"]
+__all__ = ["Sinusoid", "Chirp", "AF", "af_type_a", "af_type_b"]

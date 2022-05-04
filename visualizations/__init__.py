302a6a20cdfe4803e6b1ce1c36ca6e57c4deb2fb
carstengieshoff <carsten.gieshoff@rwth-aachen.de>
2022-05-04 11:50:31 +0200

    Add ECG visuliazation

--- visualizations/__init__.py
+++ visualizations/__init__.py
@@ -1,7 +1,8 @@
+from visualizations.plot_ecg import plot_ecg
 from visualizations.plot_fft import plot_fft
 from visualizations.plot_filter import plot_filter
 from visualizations.plot_hist import plot_hist2d
 from visualizations.plot_rp import plot_rp
 from visualizations.plot_spectral_envelope import plot_spectral_envelope
 
-__all__ = ["plot_hist2d", "plot_rp", "plot_fft", "plot_spectral_envelope", "plot_filter"]
+__all__ = ["plot_hist2d", "plot_rp", "plot_fft", "plot_spectral_envelope", "plot_filter", "plot_ecg"]
---
+++ visualizations/plot_ecg.py
@@ -0,0 +1,62 @@
+from typing import Optional
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_ecg(
+    signal: np.array,
+    r_peaks: Optional[np.array] = None,
+    q_locs: Optional[np.array] = None,
+    front: Optional[int] = None,
+    back: Optional[int] = None,
+    legend: bool = True,
+    title: Optional[str] = None,
+    show: bool = True,
+) -> None:
+    if len(signal.shape) == 1:
+        signal = signal.reshape(-1, 1)
+    signal_len, num_leads = signal.shape
+
+    fig, ax = plt.subplots(num_leads, 1, figsize=(30, 2 * num_leads))
+    for lead in range(num_leads):
+        ax[lead].plot(signal[:, lead], label="ECG")
+        if r_peaks is not None:
+            ax[lead].scatter(r_peaks, signal[r_peaks, lead], marker="+", color="red", label="R-Peak")
+            if front is not None and back is not None:
+                for s, peak in enumerate(r_peaks):
+                    ax[lead].axvspan(
+                        peak - front, peak + back, alpha=0.3, facecolor="grey", label="_" * s + "QRST-Window"
+                    )
+        if q_locs is not None:
+            ax[lead].scatter(q_locs, signal[q_locs, lead], marker="*", color="red", label="Q-loc")
+
+    if legend:
+        ax[0].legend(fontsize="x-large")
+
+    if title is not None:
+        plt.suptitle(title, fontsize="xx-large", fontweight="bold")
+
+    if show:
+        plt.show()
+
+
+if __name__ == "__main__":
+    from wfdb.processing import gqrs_detect, xqrs_detect
+
+    ecg = np.load("../data/SR1.npy")
+    Fs = 500
+    ecg = (ecg - ecg.mean(axis=0)) * 10  # /ecg.std(axis=0)
+    calc_lead = 0
+
+    q_locs = gqrs_detect(np.concatenate([ecg[:, calc_lead], ecg[:Fs, calc_lead]]), fs=Fs)
+    r_peaks = xqrs_detect(ecg[:, calc_lead], Fs, verbose=False)
+    rr_min = (r_peaks[1:] - r_peaks[:-1]).min()
+
+    plot_ecg(
+        signal=ecg[:, :4],
+        r_peaks=r_peaks[r_peaks <= ecg.shape[0]],
+        q_locs=q_locs[q_locs <= ecg.shape[0]],
+        front=25,
+        back=int(0.8 * rr_min),
+    )

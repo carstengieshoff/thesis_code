698f164ce09102058000d1759dee6097733a3fe3
carstengieshoff <carsten.gieshoff@rwth-aachen.de>
2022-06-24 10:57:54 +0200

    Add rpcombi.py

--- neural_networks/__init__.py
+++ neural_networks/__init__.py
@@ -1,7 +1,8 @@
 from neural_networks.alexnet import AlexNet
 from neural_networks.autoencoder import AutoEncoder
 from neural_networks.CNN import CNN
+from neural_networks.rpcombi import RPCombi
 from neural_networks.VAE import VAE
 from neural_networks.vgg import VGG
 
-__all__ = ["CNN", "AutoEncoder", "VAE", "AlexNet", "VGG"]
+__all__ = ["CNN", "AutoEncoder", "VAE", "AlexNet", "VGG", "RPCombi"]
